0940c1f8f7bf868ba7c9580442b3d65a7301c397
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-12-05 11:14:29 -0300

    🔨 feat: audio processing with evolution

--- lambdas/post_message/strategy.py
+++ lambdas/post_message/strategy.py
@@ -1,3 +1,6 @@
+from lambdas.post_message.extract_text_from_base64 import extract_text_from_base64
+
+
 class MessageStrategy:
     def process_message(self, message):
         raise NotImplementedError("This method should be overridden by subclasses")
@@ -12,10 +15,37 @@ class ZApiStrategy(MessageStrategy):
 
 class EvolutionStrategy(MessageStrategy):
     def process_message(self, message):
-        if not message['data']['key']['fromMe']:
-            phone_number = message['data']['key']['remoteJid'].split('@')[0]
-            text = message['data']['message']['conversation']
+        data = message.get('data', {})
+        content = data.get('message', {})
 
-            return phone_number, text
-        else:
-            return None
+        if data.get('key', {}).get('fromMe'):
+            return None, None
+
+        phone_number = self._extract_phone_number(data)
+        text = self._process_message_content(content)
+
+        return phone_number, text
+
+    @staticmethod
+    def _extract_phone_number(data):
+        return data['key']['remoteJid'].split('@')[0]
+
+    def _process_message_content(self, content):
+        if 'conversation' in content:
+            return self._extract_text(content)
+
+        if 'audioMessage' in content:
+            return self._extract_text_from_audio(content)
+
+        # TODO: O áudio dando certo, coloca aqui a lógica para processar imagens
+
+        return None
+
+    @staticmethod
+    def _extract_text(content):
+        return content.get('conversation')
+
+    @staticmethod
+    def _extract_text_from_audio(content):
+        base_64 = content.get('base64')
+        return extract_text_from_base64(base_64)
